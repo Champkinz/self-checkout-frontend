ec49c48e57dfee8d9f85f361c90b6aec301fbca7
Charana <danu_lak@live.com>
2017-10-15 20:07:50 +0530

    Adding components module

---
+++ src/app/login-customer-view/login-customer-view.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import 'hammerjs';
+
+@NgModule({
+  declarations: [
+],
+  imports: [
+    BrowserModule,
+    NgModule,
+  ],
+  providers: [],
+})
+export class AppModule { }
